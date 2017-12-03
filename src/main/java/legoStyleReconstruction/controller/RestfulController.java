6f072747fb84f684a803208bd916f4f2a6ab4112
Xinge Wen <xinge.wensxwj@gmail.com>
2017-12-02 23:22:54 -0500

    fix bug on script

--- src/main/java/legoStyleReconstruction/controller/RestfulController.java
+++ src/main/java/legoStyleReconstruction/controller/RestfulController.java
@@ -46,7 +46,7 @@ public class RestfulController {
         ProcessBuilder builder = new ProcessBuilder();
         String timeStamp = new SimpleDateFormat("yyyy-MM-dd'T'HH-mm-ss-SSSZ").format(new java.util.Date());
         ProcessBuilderHelper helper = new ProcessBuilderHelper(
-                "python demo.py ext-resources/obj-dir/"+timeStamp+".obj"+" ext-resources/resized-dir");
+                "source py3/bin/activate && python demo.py ext-resources/obj-dir/"+timeStamp+".obj"+" ext-resources/resized-dir");
         builder.command(helper.getCMD_ARRAY());
         /* the way to get home dir
            System.getProperty("user.home")
